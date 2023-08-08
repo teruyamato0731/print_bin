ab1dfa695e26d695a5250d850a1526e700af040d
Yoshikawa Teru <77840326+teruyamato0731@users.noreply.github.com>
2023-08-08 22:45:37 +0900

    style: Update print_bin.h

--- src/print_bin.h
+++ src/print_bin.h
@@ -7,16 +7,14 @@
 
 namespace rct {
 
-template<class T, std::enable_if_t<std::is_integral<T>::value, std::nullptr_t> = nullptr>
+template<class T, std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
 void print_bin(const T t) {
   using U = std::make_unsigned_t<T>;
   U mask = 1ULL << (std::numeric_limits<U>::digits - 1);
-  do {
-    printf(mask & t ? "1" : "0");
-  } while(mask >>= 1);
+  do putchar(mask & t ? '1' : '0'); while(mask >>= 1);
 }
 
-template<class T, std::enable_if_t<std::is_integral<T>::value, std::nullptr_t> = nullptr>
+template<class T, std::enable_if_t<std::is_integral_v<T>, std::nullptr_t> = nullptr>
 void put_bin(const T t) {
   putchar('0'), putchar('b'), print_bin(t), putchar('\n');
 }
